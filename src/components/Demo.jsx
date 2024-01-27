fe575ab18c2dab4e6e70a86afcd19168cf97041f
moses <echelachris@gmail.com>
2024-01-27 06:02:50 +0100

    updated logo and asset files

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -49,7 +49,7 @@ const Demo = () => {
 
 
   return (
-  <section className="mt-16 w-full max-w-xl">
+  <section className="mt-10 w-full max-w-xl">
     {/* Search */}
     <div className="flex flex-col w-full gap-2">
       <form className="relative flex justify-center items-center"
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import { logo } from '../assets'
-
+import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className="w-full flex justify-center
-    items-center flex-col">
-      <nav className="flex justify-between items-center
-       w-full mb-10 pt-3">
-        <img src={logo} alt="sumz_logo"
-          className="w-28 object-contain"        
-        />
+    <header
+      className="w-full flex justify-center
+    items-center flex-col"
+    >
+      <nav
+        className="flex justify-between items-center
+       w-full mb-10 pt-3"
+      >
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
         <button
-         type="button"
-         onClick={() => window.open("https://github.com/Mosesp4")} 
-         className="black_btn"
+          type="button"
+          onClick={() => window.open("https://github.com/Mosesp4")}
+          className="black_btn"
         >
           GitHub
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Article with <br  className="max-md:hidden"/> 
-        <span className="orange_gradient">
-          OpenAI GPT-4
-        </span>
+        Summarize Article with <br className="max-md:hidden" />
+        <span className="orange_gradient">Quickbreef</span>
       </h1>
 
       <h2 className="desc">
-        Simplify your reading with QuickBreef, an open-source article summarizer
-        that transforms lengthy articles into clear and concise briefs.
+        <span className="text-lg text-orange-500 font-bold">Introducing Quickbrief:</span> <br/> Your Personal Article Concierge! Powered by the
+        cutting-edge OpenAI GPT-4 technology, Quickbrief is not just an open-source app;
+        it's a revolution in reading efficiency. Whether it's the
+        latest news, a scientific paper, or a lengthy blog post, Quickbrief
+        distills the essence, giving you all the vital information without the
+        fluff. Say goodbye to endless scrolling and
+        hello to knowledge at your fingertips. Quickbrief – Where simplicity
+        meets brilliance, and staying informed is just a tap away!
       </h2>
     </header>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
